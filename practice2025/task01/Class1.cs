94c2d4b783e28ab697af9b61a0f512afbea87b65
Georgy_Potseluyko <georgypoc7@gmail.com>
2026-06-25 18:43:15 +0600

    feat: palindrome logic and tests

---
+++ practice2025/task01/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        input = input.ToLower();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (char.IsPunctuation(input[i]) || char.IsWhiteSpace(input[i]))
+            {
+                input = input.Remove(i, 1);
+                i--;
+            }
+        }
+
+        string reversed = new string(input.Reverse().ToArray());
+        return input == reversed;
+    }
+}
