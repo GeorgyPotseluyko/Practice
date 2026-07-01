8b98555bb75e76145dba4b47b27d1a733caf1bad
Georgy_Potseluyko <georgypoc7@gmail.com>
2026-07-01 19:50:25 +0600

    feat: attributes and tests

---
+++ practice2026/task07tests/UnitTest1.cs
@@ -0,0 +1,53 @@
+using System.Reflection;
+using Xunit;
+
+public class AttributeReflectionTests
+{
+    [Fact]
+    public void Class_HasDisplayNameAttribute()
+    {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Пример класса", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Method_HasDisplayNameAttribute()
+    {
+        var method = typeof(SampleClass).GetMethod("TestMethod");
+        var attribute = method.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Тестовый метод", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Property_HasDisplayNameAttribute()
+    {
+        var prop = typeof(SampleClass).GetProperty("Number");
+        var attribute = prop.GetCustomAttribute<DisplayNameAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal("Числовое свойство", attribute.DisplayName);
+    }
+
+    [Fact]
+    public void Class_HasVersionAttribute()
+    {
+        var type = typeof(SampleClass);
+        var attribute = type.GetCustomAttribute<VersionAttribute>();
+        Assert.NotNull(attribute);
+        Assert.Equal(1, attribute.Major);
+        Assert.Equal(0, attribute.Minor);
+    }
+
+    [Fact]
+    public void PrintTypeInfo_ReturnsCorrectInfo()
+    {
+        var type = typeof(SampleClass);
+        var info = ReflectionHelper.PrintTypeInfo(type);
+        Assert.Contains("DisplayName: Пример класса", info);
+        Assert.Contains("Version: 1.0", info);
+        Assert.Contains("Свойство: Number, DisplayName: Числовое свойство", info);
+        Assert.Contains("Метод: TestMethod, DisplayName: Тестовый метод", info);
+    }
+}
