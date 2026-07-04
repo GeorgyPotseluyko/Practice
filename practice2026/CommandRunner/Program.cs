681e2aabbc48f2e44d9bdd4c9c2f580ac138217d
Georgy_Potseluyko <georgypoc7@gmail.com>
2026-07-04 17:57:00 +0600

    feat: dll, console app, tests

---
+++ practice2026/CommandRunner/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string dllPath = Path.Combine(AppContext.BaseDirectory, "FileSystemCommands.dll");
+        
+        try
+        {
+            Assembly assembly = Assembly.LoadFrom(dllPath);
+
+            Console.WriteLine("Доступные команды: DirectorySize, FindFiles");
+            Console.Write("Введите имя команды: ");
+            string commandName = Console.ReadLine();
+
+            Console.Write("Введите путь к папке: ");
+            string path = Console.ReadLine();
+
+            string? mask = null;
+            if (commandName == "FindFiles")
+            {
+                Console.Write("Введите маску: ");
+                mask = Console.ReadLine();
+            }
+
+            foreach (Type type in assembly.GetExportedTypes())
+            {
+                if (typeof(ICommand).IsAssignableFrom(type) && type.IsClass)
+                {
+                    if (type.Name != commandName + "Command")
+                    {
+                        continue;
+                    }
+
+                    object instance;
+
+                    if (commandName == "FindFiles")
+                    {
+                        instance = Activator.CreateInstance(type, path, mask)!;
+                    }
+                    else
+                    {
+                        instance = Activator.CreateInstance(type, path)!;
+                    }
+
+                    var command = (ICommand)instance;
+                    command.Execute();
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ practice2026/FileSystemCommands/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+public class DirectorySizeCommand : ICommand
+{
+    public string Path;
+
+    public DirectorySizeCommand(string path)
+    {
+        Path = path;
+    }
+
+    public long DirectorySize()
+    {
+        if (!Directory.Exists(Path))
+        {
+            return 0;
+        }
+
+        long size = 0;
+        string[] files = Directory.GetFiles(Path, "*.*", SearchOption.AllDirectories);
+        foreach (string file in files)
+        {
+            FileInfo info = new FileInfo(file);
+            size += info.Length;
+        }
+        
+        return size;
+    }
+
+    public void Execute()
+    {
+        long size = DirectorySize();
+        Console.WriteLine($"Размер: {size} байт");
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    public string Path;
+    public string Mask;
+
+    public FindFilesCommand(string path, string mask)
+    {
+        Path = path;
+        Mask = mask;
+    }
+
+    public string[] FindFiles()
+    {
+        if (!Directory.Exists(Path))
+        {
+            return new string[0];
+        }
+
+        string[] files = Directory.GetFiles(Path, Mask, SearchOption.AllDirectories);
+        return files;
+    }
+
+    public void Execute()
+    {
+        string[] files = FindFiles();
+        foreach (string file in files)
+        {
+            Console.Write($"{file} ");
+        }
+    }
+}
