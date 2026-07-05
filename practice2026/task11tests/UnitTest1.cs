8b3ad6456ee15b85c0214d66ac0acdf415c5e9ea
Georgy_Potseluyko <georgypoc7@gmail.com>
2026-07-06 03:35:28 +0600

    feat: generation class and tests

---
+++ practice2026/task11tests/UnitTest1.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+public class CalculatorTests
+{
+    [Fact]
+    public async Task ValidString_ShouldRun()
+    {
+        string code = @"
+            public class Calculator : ICalculator 
+            {
+                public int Add(int a, int b) => a + b;
+                public int Minus(int a, int b) => a - b;
+                public int Mul(int a, int b) => a * b;
+                public int Div(int a, int b) => a / b;
+            }
+            return new Calculator();
+        ";
+        var options = ScriptOptions.Default.WithReferences(typeof(ICalculator).Assembly);
+
+        ICalculator calc = await CSharpScript.EvaluateAsync<ICalculator>(code, options);
+        int resAdd = calc.Add(10, 5);
+        int resMinus = calc.Minus(10, 5);
+        int resMul = calc.Mul(10, 5);
+        int resDiv = calc.Div(10, 5);
+
+        Assert.Equal(15, resAdd);
+        Assert.Equal(5, resMinus);
+        Assert.Equal(50, resMul);
+        Assert.Equal(2, resDiv);
+    }
+
+    [Fact]
+    public async Task MissingInterface_ShouldFail()
+    {
+        string code = @"
+            public class Calculator : ICalculator 
+            {
+                public int Add(int a, int b) => a + b;
+            }
+            return new Calculator();
+        ";
+        var options = ScriptOptions.Default.WithReferences(typeof(ICalculator).Assembly);
+
+        await Assert.ThrowsAsync<CompilationErrorException>(async () =>
+        {
+            await CSharpScript.EvaluateAsync<ICalculator>(code, options);
+        });
+    }
+
+    [Fact]
+    public async Task MathError_ShouldFail()
+    {
+        string code = @"
+            public class Calculator : ICalculator 
+            {
+                public int Add(int a, int b) { return a + b; }
+                public int Minus(int a, int b) { return a - b; }
+                public int Mul(int a, int b) { return a * b; }
+                public int Div(int a, int b) { return a / b; } 
+            }
+            return new Calculator();
+        ";
+        var options = ScriptOptions.Default.WithReferences(typeof(ICalculator).Assembly);
+
+        ICalculator calc = await CSharpScript.EvaluateAsync<ICalculator>(code, options);
+
+        Assert.Throws<DivideByZeroException>(() => calc.Div(10, 0));
+    }
+}
